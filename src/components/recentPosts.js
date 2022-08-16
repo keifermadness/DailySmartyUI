24585aa65e133d0c4ede71e92f248e286a576db8
Keifer Gilbert <keifergilbert@gmail.com>
2022-08-16 14:51:17 -0700

    added mobile styling

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,15 +1,35 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
+
+import Post from './post';
 
 class RecentPosts extends Component {
+
+    componentDidMount() {
+        this.props.fetchRecentPosts();
+    }
+
+    renderPosts = function() {
+        const posts = this.props.recentPosts.map((post, index) => {
+            if(index < 3) {
+                return (
+                    <Post type="recent" {...post} key={index}/>
+                )   
+            }
+        })
+        return posts
+    }
+
     render() {
-        return(
+        return (
             <div className="recent-posts">
-                <div className="recent-posts-wrapper">
+                <div className="recent-posts__wrapper">
                     <div className="recent-posts__heading">Recent Posts</div>
                     <ul className="recent-posts__posts">
-                        <li>Recent Post 0</li>
-                        <li>Recent Post 1</li>
-                        <li>Recent Post 2</li>
+                        {this.renderPosts()}
                     </ul>
                 </div>
             </div>
@@ -17,4 +37,10 @@ class RecentPosts extends Component {
     }
 }
 
-export default RecentPosts;
+function mapStateToProps(state) {
+    return {
+        recentPosts: state.posts.recentPosts
+    }
+}
+
+export default connect(mapStateToProps, actions)(RecentPosts);
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,26 +1,28 @@
-import React, { Component } from "react";
-import { Field, reduxForm } from 'redux-form';
+import React, { Component } from 'react';
+import { Field, reduxForm} from 'redux-form';
 
 import { withRouter } from 'react-router-dom';
 
 class SearchBar extends Component {
 
     handleFormSubmit = function({query}) {
-        console.log('trying to handle submit for query', query)
-        this.props.history.push('/results');
+        this.props.onSubmit(query);
     }
 
     renderInput(field) {
-        return <input type="text" placeholder="Search Daily Smarty" {...field.input} />
+        return <input type="text" placeholder="&#xf002; Search DailySmarty" {...field.input} />
     }
 
     render() {
 
-        const { handleSubmit } = this.props
+        const { handleSubmit } = this.props;
 
-        return(
-            <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-               <Field name="query" component={this.renderInput}/>
+        return (
+            <form className={`search-bar search-bar__${this.props.page}`} onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <div className="search-bar__wrapper">
+                    <Field name="query" component={this.renderInput}/>
+                    <p>Press return to search</p>
+                </div>
             </form>
         )
     }
